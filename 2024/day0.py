ecb9e461e133675f9f0438567df74b07f1aeb33a
zswope <zebulonswope@rocketmail.com>
2024-12-13 02:41:12 -0700

    update template to consolidate all data processing

--- 2024/day0.py
+++ 2024/day0.py
@@ -1,10 +1,10 @@
 star1 = 0
 star2 = 0
 import os
-data = open(os.path.basename(__file__).split('.')[0]+'.txt','r').readlines()
+data = open(os.path.basename(__file__).split('.')[0]+'.txt','r').read()
 # data = """
-# """.split('\n')
-data = [line.strip() for line in data if line.strip()]
+# """
+data = [line.strip() for line in data.split('\n') if line.strip()]
 
 # do computation here!
 for dat in data:
